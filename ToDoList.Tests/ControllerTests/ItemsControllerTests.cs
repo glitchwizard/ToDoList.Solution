737666ea6f73a637f406362539dafe6694f8f4a3
Charley McGowan and chris rudnicky <charley+chris@>
2018-11-28 13:07:38 -0800

    Working on adding categories to the list.

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using System;
+
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class HomeControllerTest : IDisposable
+    {
+        public void Dispose()
+        {
+            Item.ClearAll();
+        }
+
+        [TestMethod]
+        public void DontHaveATestYet()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+        }
+    }
+}
---
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using System;
+
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class CategoryTest : IDisposable
+    {
+        public void Dispose()
+        {
+            Item.ClearAll();
+        }
+
+        [TestMethod]
+        public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+        {
+            // Arrange
+            Category newCategory = new Category("test category");
+            // Act
+
+            //Assert
+            Assert.AreEqual(typeof(Category), newCategory.GetType());
+        }
+    }
+}
